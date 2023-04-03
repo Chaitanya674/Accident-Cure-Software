d96c376d3a5bcba56a3654149019459c0f6b8e80
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-04-03 19:57:51 +0530

    Accident-cure Software

--- camera.py
+++ camera.py
@@ -6,23 +6,40 @@ import os
 model = AccidentDetectionModel("model.json", 'model_weights.h5')
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-def startapplication():
-    video = cv2.VideoCapture('cars.mp4') 
+def load_model(model_file, weights_file):
+    return AccidentDetectionModel(model_file, weights_file)
+
+def process_frame(frame, model):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    roi = cv2.resize(gray_frame, (250, 250))
+    pred, prob = model.predict_accident(roi[np.newaxis, :, :])
+    return pred, prob
+
+def display_result(frame, pred, prob):
+    if pred == "Accident":
+        prob = round(prob[0][0] * 100, 2)
+        cv2.rectangle(frame, (0, 0), (280, 40), (0, 0, 0), -1)
+        cv2.putText(frame, f"{pred} {prob}", (20, 30), font, 1, (255, 255, 0), 2)
+    cv2.imshow('Video', frame)
+
+def start_application():
+    model = load_model("model.json", 'model_weights.h5')
+    video = cv2.VideoCapture('cars.mp4')
+    
     while True:
         ret, frame = video.read()
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        roi = cv2.resize(gray_frame, (250, 250))
-
-        pred, prob = model.predict_accident(roi[np.newaxis, :, :])
-        if(pred == "Accident"):
-            prob = (round(prob[0][0]*100, 2))
-            cv2.rectangle(frame, (0, 0), (280, 40), (0, 0, 0), -1)
-            cv2.putText(frame, pred+" "+str(prob), (20, 30), font, 1, (255, 255, 0), 2)
-
+        
+        if not ret:
+            break  # Exit the loop if there are no more frames
+        
+        pred, prob = process_frame(frame, model)
+        display_result(frame, pred, prob)
+        
         if cv2.waitKey(33) & 0xFF == ord('q'):
-            return
-        cv2.imshow('Video', frame)  
-
+            break  # Exit the loop if 'q' is pressed
+    
+    video.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    startapplication()
+    start_application()
